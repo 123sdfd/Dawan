e66ffb6f871d0eebc905641a249f3c7fb68112f3
khosro <khosro.hengaam@gmail.com>
2023-02-27 20:10:53 +0100

    v.1.1

--- network.tf
+++ network.tf
@@ -113,9 +113,9 @@ resource "aws_route_table_association" "public_web_route_table_association" {
 # } 
 
 
-#
-# resource "aws_route" "internet_access_web" {
-#   route_table_id         = aws_route_table.webroute.id
-#   destination_cidr_block = "0.0.0.0/0"
-#   gateway_id             = aws_internet_gateway.igw.id
-# }
+
+resource "aws_route" "internet_access_web" {
+  route_table_id         = aws_route_table.webroute.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
